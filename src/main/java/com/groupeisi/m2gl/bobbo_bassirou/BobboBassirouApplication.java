61d46c93a14cc6290b9b89dc2fd7c773e5cace45
Bobbo Bassirou <bobbopales@gmail.com>
2023-07-10 20:29:11 +0000

    first

---
+++ src/main/java/com/groupeisi/m2gl/bobbo_bassirou/BobboBassirouApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.m2gl.bobbo_bassirou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobboBassirouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobboBassirouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupeisi/m2gl/bobbo_bassirou/BobboBassirouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.m2gl.bobbo_bassirou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobboBassirouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
